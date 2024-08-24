af705c1750af3db653b0e6d44008c331978ced55
esnir <esnir74@gmail.com>
2024-08-25 00:34:44 +0900

    従業員リストの作成

--- next-front/src/features/routes/employees/components/EmployeeCard.tsx
+++ next-front/src/features/routes/employees/components/EmployeeCard.tsx
@@ -8,26 +8,30 @@ import {
   VStack,
   Icon,
   Button,
+  Wrap,
 } from "@chakra-ui/react";
-import { PhoneIcon, AddIcon, WarningIcon } from "@chakra-ui/icons";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 interface EmployeeCardProps {
   name: string;
   role: string;
-  location: string;
-  dateJoined: string;
-  interests: string[];
+  dependencies: string[];
+  qualifications: string[];
+  constraints: string[];
   avatarUrl: string;
 }
 
 const EmployeeCard: React.FC<EmployeeCardProps> = ({
   name,
   role,
-  location,
-  dateJoined,
-  interests,
+  dependencies,
+  qualifications,
+  constraints,
   avatarUrl,
 }) => {
+  const handleCardClick = () => {
+    alert(`${name}のカードがタッチされました！`);
+  };
   return (
     <Box
       p={5}
@@ -36,44 +40,55 @@ const EmployeeCard: React.FC<EmployeeCardProps> = ({
       boxShadow="sm"
       backgroundColor="white"
       maxW="md"
-      mx="auto"
+      onClick={handleCardClick}
+      cursor="pointer"
+      _hover={{ backgroundColor: "gray.50", boxShadow: "md" }}
     >
-      <HStack spacing={4}>
-        <Avatar src={avatarUrl} size="lg" />
-        <VStack align="start" spacing={1}>
-          <Text fontWeight="bold" fontSize="lg">
-            {name}
-          </Text>
+      <HStack spacing={4} justifyContent="space-between">
+        <HStack spacing={4}>
+          <Avatar src={avatarUrl} size="lg" />
+          <VStack align="start" spacing={1}>
+            <Text fontWeight="bold" fontSize="lg">
+              {name}
+            </Text>
+          </VStack>
           <Text color="gray.500">{role}</Text>
-          <HStack spacing={2} alignItems="center">
-            <PhoneIcon />
-            <Text color="gray.500" fontSize="sm">
-              {location}
-            </Text>
-          </HStack>
-          <HStack spacing={2} alignItems="center">
-            <PhoneIcon />
-            <Text color="gray.500" fontSize="sm">
-              {dateJoined}
-            </Text>
-          </HStack>
-        </VStack>
+        </HStack>
         <Button variant="ghost" colorScheme="blue" size="sm" alignSelf="start">
-          <PhoneIcon />
-          Edit
+          <DeleteIcon color="red.500" />
         </Button>
       </HStack>
-
       <Text mt={4} fontWeight="bold">
-        Interests
+        qualifications
       </Text>
-      <HStack spacing={2} mt={2}>
-        {interests.map((interest) => (
+      <Wrap spacing={2} mt={2}>
+        {qualifications.map((interest) => (
           <Badge key={interest} colorScheme="blue">
             {interest}
           </Badge>
         ))}
-      </HStack>
+      </Wrap>
+
+      <Text mt={4} fontWeight="bold">
+        constraints
+      </Text>
+      <Wrap spacing={2} mt={2}>
+        {constraints.map((constraint) => (
+          <Badge key={constraint} colorScheme="blue">
+            {constraint}
+          </Badge>
+        ))}
+      </Wrap>
+      <Text mt={4} fontWeight="bold">
+        dependencies
+      </Text>
+      <Wrap spacing={2} mt={2}>
+        {dependencies.map((dep) => (
+          <Badge key={dep} colorScheme="blue">
+            {dep}
+          </Badge>
+        ))}
+      </Wrap>
     </Box>
   );
 };
--- next-front/src/features/routes/employees/components/EmployeeList.tsx
+++ next-front/src/features/routes/employees/components/EmployeeList.tsx
@@ -4,7 +4,7 @@ import React, { FC, useState, useEffect } from "react";
 import { handleFetchEmployeesList } from "../hooks";
 import { Employee } from "../type";
 import { useAuth } from "@/state/authContext";
-import { Wrap, SimpleGrid, Center, Spinner } from "@chakra-ui/react";
+import { Text, SimpleGrid, Center, Spinner, Stack } from "@chakra-ui/react";
 import EmployeeCard from "./EmployeeCard";
 export const EmployeeList: FC = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
@@ -27,18 +27,27 @@ export const EmployeeList: FC = () => {
   }, [user]);
 
   return (
-    <>
-      <h1>EmployeeList</h1>
+    <Stack>
+      <Text fontSize="5xl">従業員一覧</Text>
       {employees ? (
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6} padding={6}>
           {employees.map((employee, index) => (
             <EmployeeCard
               key={index}
-              name={employee.firstName + " " + employee.lastName}
+              name={employee.lastName + " " + employee.firstName}
               role={employee.employeeType.typeName}
-              location={employee.firstName}
-              dateJoined={employee.firstName}
-              interests={[employee.firstName]}
+              dependencies={employee.dependencies.map(
+                (dep) =>
+                  dep.dependentEmployee.lastName +
+                  " " +
+                  dep.dependentEmployee.firstName
+              )}
+              qualifications={employee.qualifications.map(
+                (qualification) => qualification.name
+              )}
+              constraints={employee.employeeConstraints.map(
+                (constraint) => constraint.constraint.name
+              )}
               avatarUrl={employee.firstName}
             />
           ))}
@@ -49,6 +58,6 @@ export const EmployeeList: FC = () => {
           <Spinner />
         </Center>
       )}
-    </>
+    </Stack>
   );
 };
